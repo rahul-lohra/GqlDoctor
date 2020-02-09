5c17674dbb9f95f1837a7d842ba17ed656395470
rahul-lohra <rahul.lohra@tokopedia.com>
2020-02-09 22:58:46 +0530

    Update window settings

--- linux/main.cc
+++ linux/main.cc
@@ -61,8 +61,8 @@ int main(int argc, char **argv) {
   flutter::FlutterWindowController flutter_controller(icu_data_path);
   flutter::WindowProperties window_properties = {};
   window_properties.title = "Droid Db explorer";
-  window_properties.width = 800;
-  window_properties.height = 800;
+  window_properties.width = 1600;
+  window_properties.height = 1000;
 
   // Start the engine.
   if (!flutter_controller.CreateWindow(window_properties, assets_path,
