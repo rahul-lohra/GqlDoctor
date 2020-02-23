4d9140100745774c30d0ca2c5b5390dedc59b434
rahul-lohra <rahul.lohra@tokopedia.com>
2020-02-23 15:48:07 +0530

    Update logic to get adb path

--- lib/domain/usecases/GetAdbUseCase.dart
+++ lib/domain/usecases/GetAdbUseCase.dart
@@ -5,19 +5,19 @@ class GetAdbUseCase{
   String getAdbPath(){
     String baseFolder = "android-platform-tools";
     String pathSuffix = "platform-tools/adb";
-    if (Platform.isLinux) {
+    if (Platform.isMacOS) {
       return baseFolder +
           "/" +
           "platform-tools_r29.0.5-darwin" +
           "/" +
           pathSuffix;
-    } else if (Platform.isWindows) {
+    } else if (Platform.isLinux) {
       return baseFolder +
           "/" +
           "platform-tools_r29.0.5-linux" +
           "/" +
           pathSuffix;
-    } else if (Platform.isMacOS) {
+    } else if (Platform.isWindows) {
       return baseFolder +
           "/" +
           "platform-tools_r29.0.5-windows" +
